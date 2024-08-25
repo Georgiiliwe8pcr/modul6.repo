a0b00d3610a513e79afc50a946b385a97f4f7505
Georgiiiilv19 <georgijlitvinenko0@gmail.com>
2024-08-25 08:29:30 +0300

    12121

--- homework6/main.py
+++ homework6/main.py
@@ -1,27 +1,27 @@
 from address_book import AddressBook
 from record import Record
 
-# Creating a new address book
+# Створення нової адресної книги
 book = AddressBook()
-# Creating a record for John
+
 john_record = Record("John")
 john_record.add_phone("1234567890")
 john_record.add_phone("5555555555")
-# Adding John's record to the address book
+# Додавання запису Джона до адресної книги
 book.add_record(john_record)
-# Creating and adding a new record for Jane
+#Створення та додавання нового запису для Джейн
 jane_record = Record("Jane")
 jane_record.add_phone("9876543210")
 book.add_record(jane_record)
-# Printing all records in the book
+# Друк всіх записів у книзі
 for name, record in book.data.items():
     print(record)
-# Finding and editing the phone number for John
+
 john = book.find("John")
 john.edit_phone("1234567890", "1112223333")
-print(john)  # Output: Contact name: John, phones: 1112223333; 5555555555
-# Searching for a specific phone number in John's record
+print(john)  #Вихід: Контактна особа: John, телефони: 1112223333; 5555555555
+# Пошук певного номера телефону в записі Джона
 found_phone = john.find_phone("5555555555")
-print(f"{john.name}: {found_phone}")  # Output: 5555555555
-# Deleting Jane's record
+print(f"{john.name}: {found_phone}")  # вихід: 5555555555
+
 book.delete("Jane")
